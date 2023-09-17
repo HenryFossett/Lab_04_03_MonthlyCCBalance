443fdc7a0a82b4ed7ac87934197657e4831639e9
Henry Fossett <henry.fossett@gmail.com>
2023-09-17 17:53:53 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args){ double cardBalance = 5000;
+        cardBalance = cardBalance * 1.17;
+        System.out.println("Your credit card balance after one month's interest is $" + cardBalance);
+        cardBalance = cardBalance * 1.17;
+        System.out.println("Your credit card balance after two month's interest is $" + cardBalance);
+    }
+}
